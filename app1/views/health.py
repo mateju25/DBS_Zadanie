7c0a59d4229f0a8dd36806ad7f9f6c010d6e97c2
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2021-02-18 11:53:55 +0100

    Minor fix in print
    - comma removed from date
    - added error message for database error

--- app1/views/health.py
+++ app1/views/health.py
@@ -3,11 +3,14 @@ from django.http import JsonResponse
 
 # Create your views here.
 
+
 def get_uptime(request):
     query="SELECT date_trunc('second', current_timestamp - pg_postmaster_start_time()) as uptime;"
     cursor = connection.cursor()
     cursor.execute(query)
     row = cursor.fetchone()
+    if row is None:
+        return JsonResponse({"Error": "Nothing fetched from database"})
 
-    fut_json = {"pgsql": {"uptime": str(row[0])}}
+    fut_json = {"pgsql": {"uptime": str(row[0]).replace(',', '')}}
     return JsonResponse(fut_json)
