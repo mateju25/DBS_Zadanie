57fb068be3127c79b65284ce6ce9a8b82936c5aa
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2021-02-23 11:00:02 +0100

    Refaktoring

---
+++ README.md
@@ -0,0 +1,3 @@
+Tento projekt je robeny v Pythone vo frameworku Django.
+
+Link na server: http://fiit-dbs-xdelincak-app.azurewebsites.net
--- app1/views/welcome.py
+++ app1/views/welcome.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 
+# vyrenderuje welcome stranku
 def get_welcome_page(request):
     return render(request, 'welcome_page.html')
