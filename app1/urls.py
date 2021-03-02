3a0d7988eb6153b098bc12b6bde9deed4b409688
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2021-03-02 13:44:03 +0100

    Added more parameters for GET 2
    - added registration_date_lte
    - added registration_date_gte

--- app1/urls.py
+++ app1/urls.py
@@ -5,5 +5,5 @@ from app1.views import submissions
 
 urlpatterns = [
     path('health/', health.get_uptime),
-    path('ov/submissions/', submissions.get_list_from_GET)
+    path('ov/submissions/', submissions.get_list_from_get)
 ]
--- app1/views/submissions.py
+++ app1/views/submissions.py
@@ -1,12 +1,14 @@
 import re
+from math import ceil
 
 from django.http import JsonResponse
 from django.db import connection
 
+
 def is_string(pa_string):
     if pa_string is None:
         return None
-    prog = re.compile("^[a-zA-Z_., ]+$")
+    prog = re.compile("^[0-9a-zA-Z_., :-]+$")
     if prog.match(pa_string):
         return pa_string
     else:
@@ -31,7 +33,7 @@ def make_dict_from_data(pa_data):
             "br_court_name": x[1],
             "kind_name": x[2],
             "cin": x[3],
-            "registration_date": x[4],
+            "registration_date": str(x[4]),
             "corporate_body_name": x[5],
             "br_section": x[6],
             "br_insertion": x[7],
@@ -47,9 +49,9 @@ def extract_data_from_get(request, pa_key, def_value, pa_is_number=True):
     temp = request.GET.get(pa_key, def_value)
     if pa_is_number:
         if is_number(temp) is not None:
-            return is_number(temp)
+            return int(is_number(temp))
         else:
-            return int(def_value)
+            return def_value
     else:
         if is_string(temp) is not None:
             return is_string(temp)
@@ -57,14 +59,16 @@ def extract_data_from_get(request, pa_key, def_value, pa_is_number=True):
             return def_value
 
 
-def get_list_from_GET(request):
+def get_list_from_get(request):
     page = extract_data_from_get(request, "page", "1")
     per_page = extract_data_from_get(request, "per_page", "10")
     order_by = extract_data_from_get(request, "order_by", None, pa_is_number=False)
     order_type = extract_data_from_get(request, "order_type", None, pa_is_number=False)
     corporate_body_name = extract_data_from_get(request, "corporate_body_name", None, pa_is_number=False)
-    cin = extract_data_from_get(request, "cin", None, pa_is_number=False)
+    cin = extract_data_from_get(request, "cin", None)
     city = extract_data_from_get(request, "city", None, pa_is_number=False)
+    registration_date_lte = (extract_data_from_get(request, "registration_date_lte", None, pa_is_number=False)).split()[0]
+    registration_date_gte = (extract_data_from_get(request, "registration_date_gte", None, pa_is_number=False)).split()[0]
 
     cursor = connection.cursor()
     columns = ["id", "br_court_name", "kind_name", "cin", "registration_date", "corporate_body_name",
@@ -76,15 +80,35 @@ def get_list_from_GET(request):
         else:
             query = query + x + ", "
     query = query + "FROM ov.or_podanie_issues "
+    count_query = "SELECT COUNT(id) FROM ov.or_podanie_issues "
 
     if corporate_body_name is not None or cin is not None or city is not None:
-        query = query + "WHERE 2 = 1"
+        count_query = count_query + "WHERE (2 = 1"
+        query = query + "WHERE (2 = 1"
         if corporate_body_name is not None:
             query = query + " OR corporate_body_name = '" + corporate_body_name + "' "
+            count_query = count_query + " OR corporate_body_name = '" + corporate_body_name + "' "
         if cin is not None:
-            query = query + " OR cin = " + cin + " "
+            query = query + " OR cin = " + str(cin) + " "
+            count_query = count_query + " OR cin = " + str(cin) + " "
         if city is not None:
             query = query + " OR city = '" + city + "' "
+            count_query = count_query + " OR city = '" + city + "' "
+
+        count_query = count_query + ") "
+        query = query + ") "
+
+    if registration_date_lte is not None or registration_date_gte is not None:
+        count_query = count_query + "AND ( 1 = 1 "
+        query = query + "AND ( 1 = 1 "
+        if registration_date_lte is not None:
+            query = query + "AND registration_date <= '" + registration_date_lte + "' "
+            count_query = count_query + "AND registration_date <= '" + registration_date_lte + "' "
+        if registration_date_gte is not None:
+            query = query + "AND registration_date >= '" + registration_date_gte + "' "
+            count_query = count_query + "AND registration_date >= '" + registration_date_gte + "' "
+        count_query = count_query + ") "
+        query = query + ") "
 
     if order_by is not None and order_by in columns:
         query = query + "ORDER BY " + order_by + " "
@@ -92,15 +116,14 @@ def get_list_from_GET(request):
         query = query + order_type + " "
 
     query = query + "LIMIT " + str(per_page) + " "
-    query = query + "OFFSET " + str(page * per_page) + ";"
+    query = query + "OFFSET " + str(page * per_page)
 
     cursor.execute(query)
     row = cursor.fetchall()
 
-    # query = "SELECT COUNT(id) FROM ov.or_podanie_issues;"
-    # cursor.execute(query)
-    # count = cursor.fetchone()
+    cursor.execute(count_query)
+    count = cursor.fetchone()
 
-    #metadata = {"page": page, "per_page": per_page, "pages": int(ceil(count[0]/per_page)), "total": count[0]}
-    metadata = {"page": page, "per_page": per_page, "pages": "?", "total": "?"}
+    metadata = {"page": page, "per_page": per_page, "pages": int(ceil(count[0]/per_page)), "total": count[0]}
+    #metadata = {"page": page, "per_page": per_page, "pages": "?", "total": "?"}
     return JsonResponse({"items": make_dict_from_data(row), "metadata": metadata})
