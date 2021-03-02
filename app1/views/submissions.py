9775e577234116876cebfbfc4708392450ad6bb7
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2021-03-02 09:40:24 +0100

    Basic GET function
    - added parameter page and per_page

--- app1/views/submissions.py
+++ app1/views/submissions.py
@@ -4,42 +4,78 @@ from django.http import JsonResponse
 from django.db import connection
 
 
-def is_string(paString):
+def is_string(pa_string):
     prog = re.compile("^[a-zA-Z_]+$")
-    return prog.match(paString)
+    if prog.match(pa_string):
+        return pa_string
+    else:
+        return None
 
 
-def is_number(paString):
+def is_number(pa_string):
     prog = re.compile("^[0-9]+$")
-    return int(prog.match(paString))
+    if prog.match(pa_string):
+        return int(pa_string)
+    else:
+        return None
+
+
+def make_dict_from_data(pa_data):
+    result = []
+    for x in pa_data:
+        result.append({
+            "id": x[0],
+            "br_court_name": x[1],
+            "kind_name": x[2],
+            "cin": x[3],
+            "registration_date": x[4],
+            "corporate_body_name": x[5],
+            "br_section": x[6],
+            "br_insertion": x[7],
+            "text": x[8],
+            "street": x[9],
+            "postal_code": x[10],
+            "city": x[11]
+        })
+    return result
 
 
 def get_list_from_GET(request):
-    page = request.GET["page"]
+    page = request.GET.get("page", '1')
     page = is_number(page)
     if page is None:
-        page = 1
-    per_page = request.GET["per_page"]
+        return JsonResponse({"Error" : "Volam sandare."})
+
+    per_page = request.GET.get("per_page", '10')
     per_page = is_number(per_page)
     if per_page is None:
-        per_page = 1;
-    # order_by = request.GET["order_by"]
-    # order_by = is_number(order_by)
-    # order_type = request.GET["order_type"]
-    # order_type = is_number(order_type)
+        return JsonResponse({"Error": "Volam sandare."})
 
     cursor = connection.cursor()
-    query = "SELECT " + "id, " + "br_court_name, " + "kind_name, " + "cin, " + "registration_date, " + "corporate_body_name, " + "br_section, "
-    "br_insertion, "
-    "text, "
-    "street, "
-    "postal_code, "
-    "city"
-    "FROM ov.or_podania_issues"
-    "LIMIT " + str(per_page) + ";"
-
+    query = "SELECT " \
+            + "id, " \
+            + "br_court_name, " \
+            + "kind_name, " \
+            + "cin, " \
+            + "registration_date, " \
+            + "corporate_body_name, " \
+            + "br_section, " \
+            + "br_insertion, " \
+            + "text, " \
+            + "street, " \
+            + "postal_code, " \
+            + "city " \
+            + "FROM ov.or_podanie_issues "
+    query = query + "LIMIT " + str(per_page)
+    query = query + "OFFSET " + str(page * per_page) + ";"
 
     cursor.execute(query)
-    row = cursor.fetchone()
+    row = cursor.fetchall()
 
-    return JsonResponse("Halo")
+    # query = "SELECT COUNT(id) FROM ov.or_podanie_issues;"
+    # cursor.execute(query)
+    # count = cursor.fetchone()
+
+    #metadata = {"page": page, "per_page": per_page, "pages": int(ceil(count[0]/per_page)), "total": count[0]}
+    metadata = {"page": page, "per_page": per_page, "pages": "?", "total": "?"}
+    return JsonResponse({"items": make_dict_from_data(row), "metadata": metadata})
