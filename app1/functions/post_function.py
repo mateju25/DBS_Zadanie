9b1ab2b26144a8a292c918bac4042c46cc102d96
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2021-03-03 17:08:19 +0100

    Required parameters added for POST

--- app1/functions/post_function.py
+++ app1/functions/post_function.py
@@ -3,47 +3,47 @@ from django.http import JsonResponse
 from django.http import HttpResponse
 from app1.functions.sql_defense import *
 
+import json
+
 
 def http_error_422():
     return JsonResponse({"Error": "volam sandare"})
 
 
-def verify_parameter(request, pa_key, errors, pa_is_number=False):
-    reasons = []
-    temp = request.POST.get(pa_key, -1)
-    if temp == -1:
-        reasons.append("required")
-    else:
-        if pa_is_number:
-            if is_number(temp) is not None:
-                temp = int(is_number(temp))
-            else:
-                reasons.append("not_number")
+def verify_parameter(pa_json, pa_key, errors, pa_is_number=False):
+    temp = pa_json[pa_key]
+    if pa_is_number:
+        if is_number(temp) is not None:
+            return int(is_number(temp))
         else:
-            if is_string(temp) is not None:
-                temp = is_string(temp)
-            else:
-                reasons.append("required")
-
-    if len(reasons) != 0:
-        errors.append({"field": pa_key, "reasons": reasons})
-
-    return temp
+            return None
+    else:
+        if is_string(temp) is not None:
+            return is_string(temp)
+        else:
+            return None
 
 
 def post_new_data(request):
     errors = []
-    br_court_name = verify_parameter(request, "br_court_name", errors)
-    kind_name = verify_parameter(request, "kind_name", errors)
-    cin = verify_parameter(request, "cin", errors, pa_is_number=True)
-    registration_date = verify_parameter(request, "registration_date", errors)
-    corporate_body_name = verify_parameter(request, "corporate_body_name", errors)
-    br_section = verify_parameter(request, "br_section", errors)
-    br_insertion = verify_parameter(request, "br_insertion", errors)
-    text = verify_parameter(request, "text", errors)
-    street = verify_parameter(request, "street", errors)
-    postal_code = verify_parameter(request, "postal_code", errors)
-    city = verify_parameter(request, "city", errors)
+    post_json = json.loads(request.body)
+
+    required = ["br_court_name", "kind_name", "cin", "registration_date", "corporate_body_name",
+                "br_section", "br_insertion", "text", "street", "postal_code", "city"]
+
+    for x in required:
+        if x not in post_json:
+            errors.append({"field": x, "reasons": "required"})
+        else:
+            if x == 'cin':
+                post_json[x] = verify_parameter(post_json, x, errors, pa_is_number=True)
+                if post_json[x] is None:
+                    errors.append({"field": x, "reasons": "not_number"})
+            else:
+                post_json[x] = verify_parameter(post_json, x, errors)
+                if post_json[x] is None:
+                    errors.append({"field": x, "reasons": "required"})
+
     if len(errors) != 0:
         return JsonResponse({"errors": errors})
-    return
+    return JsonResponse({"errors": errors})
