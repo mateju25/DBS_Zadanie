8cccbac1fa327cec1206c0f8d71c5f54a6392d5d
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2021-03-06 08:02:56 +0100

    added paralelism for get_function

--- app1/models/get_function.py
+++ app1/models/get_function.py
@@ -1,5 +1,7 @@
 import datetime
+import time
 from math import ceil
+import threading
 
 from django.http import JsonResponse
 from django.db import connection
@@ -15,9 +17,56 @@ def extract_and_validate_data_from_get(request, pa_key, def_value):
         return def_value
 
 
+break_thread = 2
+result = None
+row = None
+
+
+# vrati metadata
+def get_metadata(query, params):
+    global break_thread, row
+    cursor = connection.cursor()
+    if len(params) != 0:
+        cursor.execute(query, params)
+    else:
+        cursor.execute(query)
+    print("Metadata ", end="")
+    print(cursor.query)
+    row = cursor.fetchone()
+    break_thread-=1
+
+
+# vrati data pre hlavny response
+def get_data_from_database(query, params):
+    global break_thread, result
+    cursor = connection.cursor()
+    cursor.execute(query, params)
+    print("Data ", end="")
+    print(cursor.query)
+    result = cursor.fetchall()
+    break_thread-=1
+
+
+# refreshuje spojenie
+def refresh_database():
+    global break_thread
+    cursor = connection.cursor()
+    while break_thread != 0:
+        for i in range(0, 120):
+            time.sleep(1)
+            if break_thread == 0:
+                print("Vypinam")
+                return
+        cursor.execute("SELECT id FROM ov.likvidator_issues LIMIT 1;")
+        print("Refresh", end='')
+        print(cursor.query)
+        res = cursor.fetchone()
+
+
 # vrati json s datami, ktore odpovedaju parametrom z pola GET
 def get_list_from_get(request):
     # v dictionary params sa budu nachadzat dane argumenty na filtrovanie
+    global break_thread, row
     params = {}
     params["page"] = extract_and_validate_data_from_get(request, "page", "1")
     params["per_page"] = extract_and_validate_data_from_get(request, "per_page", "10")
@@ -45,7 +94,7 @@ def get_list_from_get(request):
     try:
         if p_registration_date_lte is not None:
             datetime.datetime.strptime(p_registration_date_lte, '%Y-%m-%d %H:%M:%S.%f')
-            params["registration_date_lte"] = p_registration_date_lte
+            params["registration_date_lte"] = (p_registration_date_lte.split())[0]
     except ValueError:
         pass
 
@@ -54,11 +103,9 @@ def get_list_from_get(request):
     try:
         if p_registration_date_gte is not None:
             datetime.datetime.strptime(p_registration_date_gte, '%Y-%m-%d %H:%M:%S.%f')
-            params["registration_date_gte"] = p_registration_date_gte
+            params["registration_date_gte"] = (p_registration_date_gte.split())[0]
     except ValueError:
         pass
-
-    cursor = connection.cursor()
 
     # vrati hlavne data
     query_params = ()
@@ -82,21 +129,22 @@ def get_list_from_get(request):
 
     query += where_clause + order_by_string
     query_params += (int(params["per_page"]), ((int(params["page"]) - 1) * int(params["per_page"])),)
-    cursor.execute(query, query_params)
-    print(cursor.query)
-    row = cursor.fetchall()
 
-    #zisti metadata
+    t1 = threading.Thread(target=get_data_from_database, args=(query, query_params,))
+    t2 = threading.Thread(target=refresh_database)
     query = "SELECT COUNT(id) FROM ov.or_podanie_issues " + where_clause
     query_params = query_params[0:-2]
-    if len(query_params) != 0:
-        cursor.execute(query, query_params)
-    else:
-        cursor.execute(query)
-    print(cursor.query)
-    count = cursor.fetchone()
+    t3 = threading.Thread(target=get_metadata, args=(query, query_params,))
+
+    t1.start()
+    t2.start()
+    t3.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
 
     # vytvori metadata
     metadata = {"page": int(params["page"]), "per_page": int(params["per_page"]),
-                "pages": int(ceil(count[0]/int(params["per_page"]))), "total": count[0]}
-    return JsonResponse({"items": make_dict_from_data(row), "metadata": metadata}, status=200)
+                "pages": int(ceil(row[0]/int(params["per_page"]))), "total": row[0]}
+    return JsonResponse({"items": make_dict_from_data(result), "metadata": metadata}, status=200)
