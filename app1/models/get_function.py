26a57caed8682ffe3092d097fb1624d87a2f41aa
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2021-03-06 09:58:55 +0100

    Revert "second try to overcome timeout"
    This reverts commit d5cac3e5daafe47ca041438c1ff1db914ac83615.

--- app1/models/get_function.py
+++ app1/models/get_function.py
@@ -86,30 +86,17 @@ def get_list_from_get(request):
     print(cursor.query)
     row = cursor.fetchall()
 
-    #zisti najvyssie id ake je pre dane podmienky
-    query = "SELECT id FROM ov.or_podanie_issues " + where_clause + """ ORDER BY id desc LIMIT 1"""
+    #zisti metadata
+    query = "SELECT COUNT(id) FROM ov.or_podanie_issues " + where_clause
     query_params = query_params[0:-2]
     if len(query_params) != 0:
         cursor.execute(query, query_params)
     else:
         cursor.execute(query)
-    count = (cursor.fetchone())[0]
-    print(count)
-
-    count_real = 0
-    #zisti metadata
-    for i in range(0, count+10, int(count/10)):
-        if len(where_clause) == 0:
-            id_clause = """WHERE (id >= %s AND id <= %s)"""
-        else:
-            id_clause = """AND (id >= %s AND id <= %s)"""
-        query = """SELECT COUNT(id) FROM ov.or_podanie_issues """ + where_clause + id_clause
-        query_params = query_params[0:-2] + (i, i+int(count/10), )
-        cursor.execute(query, query_params)
-        print(cursor.query)
-        count_real += (cursor.fetchone())[0]
+    print(cursor.query)
+    count = cursor.fetchone()
 
     # vytvori metadata
     metadata = {"page": int(params["page"]), "per_page": int(params["per_page"]),
-                "pages": int(ceil(count_real/int(params["per_page"]))), "total": count_real}
+                "pages": int(ceil(count[0]/int(params["per_page"]))), "total": count[0]}
     return JsonResponse({"items": make_dict_from_data(row), "metadata": metadata}, status=200)
