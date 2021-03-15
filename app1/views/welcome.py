f2215924d2b18f2696a00da7c73ef729d809e6e6
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2021-03-15 11:47:27 +0100

    deleted welcome page

--- app1/views/welcome.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-# vyrenderuje welcome stranku
-def get_welcome_page(request):
-    return render(request, 'welcome_page.html')
--- azure/urls.py
+++ azure/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from app1.views import welcome
 
 urlpatterns = [
     path('v1/', include('app1.urls')),
-    path('', welcome.get_welcome_page),
     path('admin/', admin.site.urls),
 ]
